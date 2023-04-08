be229a533783550421c3038af35c4d4042279a04
drogon98 <h21948298@gmail.com>
2023-04-08 21:03:19 +0300

    added redux persist

--- store/index.ts
+++ store/index.ts
@@ -1,14 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
 import { authReducer } from "./slices/auth";
 import { todosReducer } from "./slices/todos";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const todosPersistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["todos"],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  todos: persistReducer(todosPersistConfig, todosReducer),
+});
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    todos: todosReducer,
-  },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
